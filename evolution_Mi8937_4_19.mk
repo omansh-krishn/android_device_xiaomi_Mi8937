5a48fa5f6070ca0ba1664f9ab63cef4ac2254bb3
Omansh Krishn <96343317+omansh-krishn@users.noreply.github.com>
2023-08-04 20:32:04 +0530

    evolution-x

--- evolution_Mi8937_4_19.mk
+++ evolution_Mi8937_4_19.mk
@@ -9,8 +9,45 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Evolution-X stuff.
+$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
+-include device/evolution/sepolicy/qcom/sepolicy.mk
+
+# OTA
+EVO_OTA_VERSION_CODE := thirteen
+PRODUCT_PRODUCT_PROPERTIES += \
+    org.evolution.ota.version_code=$(EVO_OTA_VERSION_CODE)
+
+# FLAGS
+EVO_BUILD_TYPE := COMMUNITY-OmanshKrishn
+EVO_SIGNED := true
+TARGET_SUPPORTS_TOUCHGESTURES := true
+TARGET_BUILD_APERTURE_CAMERA := true
+TARGET_ENABLE_BLUR := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_PIXEL_STAND_SUPPORTED := true
+TARGET_IS_PIXEL_7 := true
+TARGET_IS_PIXEL := true
+TARGET_USES_PICO_GAPPS := true
+TARGET_BOOT_ANIMATION_RES := 1080
+ifeq ($(NEED_GCAM), true)
+PRODUCT_PACKAGES += \
+    GoogleCamera \
+    SCONE
+endif
+
+ifeq ($(pixelify), true)
+PRODUCT_PACKAGES += \
+    NowPlayingOverlay \
+    PixelBatteryHealthOverlay \
+    PixelDisableLEDOverlay \
+    PixelSetupWizardOverlay2019 \
+    PixelSetupWizardOverlay2021 \
+    ClearCallingSettingsOverlay2022 \
+    PixelConnectivityOverlay2022 \
+    DreamlinerOverlay
+endif
 
 # Kernel
 TARGET_KERNEL_VERSION := 4.19
